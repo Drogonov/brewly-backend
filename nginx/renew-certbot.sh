b80a67025b0f7f66777b36c6890896ed3d126785
Anton Vlezko <anton@vlezko.com>
2025-06-12 17:53:03 +0300

    Add small fix

--- nginx/renew-certbot.sh
+++ nginx/renew-certbot.sh
@@ -9,14 +9,16 @@ PROJECT_ROOT="$SCRIPT_DIR"
 WEBROOT="$PROJECT_ROOT/certbot/www"
 CONF_DIR="$PROJECT_ROOT/certbot/conf"
 
-# Determine Docker network ID for nginx/app
-NETWORK_ID=$(docker network ls --filter name=brewly-backend_default -q)
+# Use your Compose-created network name. By default it will be:
+#   <project>_brewly_net
+# If your project folder is named "brewly-backend", that's:
+NETWORK_NAME="brewly-backend_brewly_net"
 
 # Run Certbot to force-renew certificates
 docker run --rm \
   -v "$WEBROOT:/var/www/certbot" \
   -v "$CONF_DIR:/etc/letsencrypt" \
-  --network "$NETWORK_ID" \
+  --network "$NETWORK_NAME" \
   certbot/certbot certonly \
     --webroot \
     --webroot-path=/var/www/certbot \
