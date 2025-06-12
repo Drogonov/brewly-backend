556d7b9398a96e16c1da98ff9a24380357b0955d
Anton Vlezko <anton@vlezko.com>
2025-06-12 18:13:32 +0300

    One more try to fix nginx

--- nginx/docker-entrypoint.d/51-wait-for-cert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CERT=/etc/letsencrypt/live/brewly.ru/fullchain.pem
-
-if [ ! -f "$CERT" ]; then
-  echo "[entrypoint] waiting for real cert to appear…"
-  while [ ! -f "$CERT" ]; do
-    sleep 1
-  done
-fi
--- nginx/docker-entrypoint.d/52-fix-perms.sh
+++ nginx/docker-entrypoint.d/52-fix-perms.sh
@@ -1,6 +1,6 @@
-# docker-entrypoint.d/52-fix-perms.sh
 #!/bin/sh
-# Ensure nginx (user “nginx”) can read all files under /etc/letsencrypt
+# Ensure nginx (user “nginx”) can read /etc/letsencrypt
 if [ -d /etc/letsencrypt ]; then
   chmod -R a+rX /etc/letsencrypt
 fi
+exec /docker-entrypoint.sh "$@"
