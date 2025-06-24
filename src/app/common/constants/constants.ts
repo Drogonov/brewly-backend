7ce4415e61264494dc7c0be0adfbe04ac639de94
Anton Vlezko <anton@vlezko.com>
2025-06-24 21:08:43 +0300

    Token lifetime fix

--- src/app/common/constants/constants.ts
+++ src/app/common/constants/constants.ts
@@ -1,6 +1,5 @@
-export const ACCESS_TOKEN_EXPIRATION = '7d';
+export const ACCESS_TOKEN_EXPIRATION = '15m';
 export const REFRESH_TOKEN_EXPIRATION = '7d';
 export const JWT = 'jwt';
 export const JWT_REFRESH = 'jwt-refresh';
 export const IS_PUBLIC = 'isPublic';
-
